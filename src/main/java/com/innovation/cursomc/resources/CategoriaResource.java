4786c87d9865abb4a92c91fb44c3be70e5a4a6f4
Flavio Oliveira <ff_oliveira@hotmail.com>
2020-12-09 22:45:46 -0300

    primeira classe dominio

--- src/main/java/com/innovation/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/innovation/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.innovation.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.innovation.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Esse rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Escritório");
+		Categoria cat2 = new Categoria(2,"Informática");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		
+		return lista;
 		
 	}
 }
