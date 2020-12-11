095ac9653af0ded898e57e84e896ed37ca3bd3f8
Flavio Oliveira <ff_oliveira@hotmail.com>
2020-12-11 20:12:35 -0300

    ajuste Json Ignore

--- src/main/java/com/innovation/cursomc/domain/Cidade.java
+++ src/main/java/com/innovation/cursomc/domain/Cidade.java
@@ -25,7 +25,6 @@ public class Cidade implements Serializable{
 	private Integer id;
 	private String nome;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
