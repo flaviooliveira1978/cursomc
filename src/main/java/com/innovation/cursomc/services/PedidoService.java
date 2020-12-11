68c5c0df0c8940d212aa4d9e29d8a4583a06598d
Flavio Oliveira <ff_oliveira@hotmail.com>
2020-12-11 18:58:37 -0300

    resource pedidos

---
+++ src/main/java/com/innovation/cursomc/services/PedidoService.java
@@ -0,0 +1,34 @@
+package com.innovation.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.innovation.cursomc.domain.Pedido;
+import com.innovation.cursomc.eexceptions.ObjectNotFoundException;
+import com.innovation.cursomc.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		
+		if (obj ==null) {
+			throw new ObjectNotFoundException("Pedido não encontrado. Id: "+ id);
+
+		}
+		else { 
+		
+		/*
+		 * return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		 */
+		
+		return obj.get(); 
+		}
+	}
+}
